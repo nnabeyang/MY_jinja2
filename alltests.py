59daeb7a6339a1d125e0fc69589dd7cfc7f007e1
noriaki watanabe <nabeyang@gmail.com>
2011-07-25 10:05:06 +0900

    renamed file names of testcase

--- alltests.py
+++ alltests.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2.6
 import unittest
 from test import test_support
-from MY_jinja2_environment_test import TemplateTests, EnvironmentTests
-from MY_jinja2_nodes_test import NodeTests
-from MY_jinja2_lexer_test import TokenTests
-from MY_jinja2_parser_test import ParserTests
-from MY_jinja2_compiler_test import CompilerTests
-from MY_jinja2_runtime_test import ContextTests
+from environment import TemplateTests, EnvironmentTests
+from nodes import NodeTests
+from lexer import TokenTests
+from parser import ParserTests
+from compiler import CompilerTests
+from runtime import ContextTests
 from jinja2_api_test import FromString_jinja2Tests, FromString_MY_jinja2Tests
 if __name__ == '__main__':
   test_support.run_unittest(TemplateTests,EnvironmentTests,
